24993d0e185ff41c7720f05c05c0a9e6a48543f6
Max <tan91572@gapps.uwcsea.edu.sg>
2024-07-20 17:49:14 +0800

    fix the bug of footer

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("static")); // Serve static files from the "static" directory
 
-var title;
-var content;
+
 let contentHolder = []
 let titleHolder = []
+var SearchTitle;
 
 
 
@@ -20,10 +20,7 @@ app.post("/view",(req,res)=>{
     // Only push non-empty values
     if (title) titleHolder.push(title);
     if (content) contentHolder.push(content);
-    res.render("view.ejs", {
-        contentHolder: contentHolder,
-        titleHolder: titleHolder
-      });
+    res.redirect("/view");
       console.log(req.body);
       console.log(contentHolder);
       console.log(titleHolder);
