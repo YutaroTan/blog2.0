b6d474d838f48e59218a0548b879aa2c667fe5d0
Max <tan91572@gapps.uwcsea.edu.sg>
2024-07-20 17:07:35 +0800

    work on back end (partials)

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("static")); // Serve static files from the "static" directory
 
+var title;
+var content;
+let contentHolder = []
+let titleHolder = []
+
+function getTitle(req,res,next){
+    title = req.body["title:"];
+    titleHolder.push(title);
+    next();
+}
+
+function getContent(req,res,next){
+    content = req.body["content"];
+    contentHolder.push(content);
+    next();
+}
+
+
+app.use(getTitle);
+app.use(getContent);
+
+app.post("/view",(req,res)=>{
+    res.render("view.ejs", {
+        contentHolder: contentHolder,
+        titleHolder: titleHolder
+      });
+      console.log(req.body);
+      console.log(contentHolder);
+      console.log(titleHolder);
+});
+
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
@@ -15,7 +46,10 @@ app.get("/post",(req,res)=>{
 });
 
 app.get("/view",(req,res)=>{
-    res.render("view.ejs");
+    res.render("view.ejs", {
+        contentHolder: contentHolder,
+        titleHolder: titleHolder
+      });
 });
 
 app.listen(port, () => {
